051b2ba3ab6bc5088264008a4c89cf1dac4dd249
Gustav Pursche <gustavpursche@gmail.com>
2014-08-20 18:20:04 +0200

    add custom post types for work

--- functions.php
+++ functions.php
@@ -1,3 +1,40 @@
 <?php
   add_theme_support( 'post-thumbnails' );
+  add_action( 'init', 'create_post_types' );
+
+  function create_post_types() {
+    register_post_type( 'work_gallery',
+      array(
+        'labels' => array(
+          'name' => __( 'Work Gallery' ),
+          'singular_name' => __( 'Work Gallery' )
+        ),
+      'public' => true,
+      'has_archive' => false,
+      )
+    );
+
+    register_post_type( 'work_text',
+      array(
+        'labels' => array(
+          'name' => __( 'Work Text' ),
+          'singular_name' => __( 'Work Text' )
+        ),
+      'public' => true,
+      'has_archive' => false,
+      )
+    );
+
+    register_post_type( 'work_video',
+      array(
+        'labels' => array(
+          'name' => __( 'Work Video' ),
+          'singular_name' => __( 'Work Video' )
+        ),
+      'public' => true,
+      'has_archive' => false,
+      )
+    );
+  }
+
 ?>
