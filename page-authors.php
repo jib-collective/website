8d39352cc1c3e21b1bc841b996aa1d3faf194abb
Gustav Pursche <gustavpursche@gmail.com>
2014-09-13 18:52:45 +0200

    enhance styling of authors page

--- page-authors.php
+++ page-authors.php
@@ -7,26 +7,37 @@
 <div class="app_content-limiter">
   <h1 class="page_headline">Authors</h1>
 
+  <ul class="author_list">
+
   <?php
     foreach ( $users as $user ) {
       $userdata = get_user_meta( $user->ID );
       $bio = $userdata['biography'][0];
       $url = get_author_posts_url( $user->ID );
+      $image_url = get_field( $userdata['_image'][0], 'user_' . $user->ID );
       ?>
 
-      <h2>
-        <a href="<?php echo $url; ?>">
-          <?php echo $user->display_name; ?>
-        </a>
-      </h2>
+      <li class="author">
+        <img src="<?php echo $image_url; ?>" class="author_image" />
 
-      <p>
-        <?php echo $bio; ?>
-      </p>
+        <div class="author_data">
+          <h2 class="author_title">
+            <a href="<?php echo $url; ?>">
+              <?php echo $user->display_name; ?>
+            </a>
+          </h2>
+
+          <div class="richtext">
+            <?php echo $bio; ?>
+          </div>
+        </div>
+      </li>
 
       <?php
     }
   ?>
+
+  </ul>
 </div>
 
 <?php get_footer(); ?>
