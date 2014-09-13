ffcfe1836c7a14906c41170faf0f9bb06167d0c8
Gustav Pursche <gustavpursche@gmail.com>
2014-09-13 19:17:43 +0200

    add content limiter to category pages

--- category-work.php
+++ category-work.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-<div class="grid">
+<div class="grid app_content-limiter">
   <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
     <div class="post">
       <?php
