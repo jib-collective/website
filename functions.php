664277c91c51bbf392ced7ac820a4ea15112a5b8
Gustav Pursche <gustavpursche@gmail.com>
2014-09-13 14:32:03 +0200

    enable navigation menus again

--- functions.php
+++ functions.php
@@ -5,4 +5,10 @@
   add_image_size( 'featured-category-image', 700 );
   add_image_size( 'category-image', 350 );
 
+  function register_my_menu() {
+    register_nav_menu( 'header-menu', __( 'Header Menu' ) );
+  }
+
+  add_action( 'init', 'register_my_menu' );
+
 ?>
