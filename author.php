0353e285a3471d839d5a6bb72931ce436d5ec7da
Gustav Pursche <gustavpursche@gmail.com>
2014-09-13 20:04:20 +0200

    refactor front-page to show featured articles at the beginning

--- author.php
+++ author.php
@@ -1,19 +1,21 @@
 <?php get_header(); ?>
 
-<?php
-  $id = the_author_meta( 'ID' );
-  $userdata = get_user_meta( $id );
-  $bio = $userdata['biography'];
+<div class="page app_content-limiter">
+  <?php
+    $id = the_author_meta( 'ID' );
+    $userdata = get_user_meta( $id );
+    $bio = $userdata['biography'];
 
-  $posts = query_posts( 'author=' + $id );
-?>
+    $posts = query_posts( 'author=' + $id );
+  ?>
 
-<h1>
-  <?php the_author() ?>
-</h1>
+  <h1 class="page_headline">
+    <?php the_author() ?>
+  </h1>
 
-<p>
-  <?php echo $bio; ?>
-</p>
+  <p class="richtext">
+    <?php echo $bio; ?>
+  </p>
+</div>
 
 <?php get_footer(); ?>
--- front-page.php
+++ front-page.php
@@ -1,41 +1,77 @@
 <?php get_header(); ?>
 
+<?php
+
+  $posts = query_posts('category=1');
+  wp_reset_query();
+?>
+
 <div class="grid app_content-limiter">
-  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-    <?php
-      $featured_class = "";
+  <?php
+    /* show last featured post */
+    foreach ( $posts as $post  ) {
+      $post_id = $post->ID;
       $is_featured = false;
+      $acf_fields = get_field_objects( $post_id );
+      $excerpt = get_field( 'excerpt', $post_id );
 
-      if( has_tag( 'featured' ) ) {
-        $featured_class = "post--featured";
-        $is_featured = true;
-      }
-    ?>
+      if( has_tag( 'featured', $post ) ) {
+  ?>
 
-    <div class="post <?php echo $featured_class ?>">
-      <a href="<?php the_permalink(); ?>">
-        <?php
-          if ( has_post_thumbnail() ) {
-            if( $is_featured ) {
-              the_post_thumbnail( 'featured-category-image', array( "class" => "post_image" ));
-            } else {
-              the_post_thumbnail( 'category-image', array( "class" => "post_image" ));
-            }
-          }
-        ?>
+    <div class="post post--featured">
+      <?php
+        if( has_post_thumbnail( $post_id ) ) {
+          echo get_the_post_thumbnail( $post_id, 'category-image', array( "class" => "post_image" ));
+        }
+      ?>
 
-        <div class="post_meta">
+      <div class="post_meta">
           <h2 class="post_headline">
-            <?php the_title(); ?>
+            <?php echo $post->post_title; ?>
           </h2>
 
           <div class="post_excerpt richtext">
-            <?php the_excerpt(); ?>
+            <?php echo $excerpt; ?>
           </div>
         </div>
-      </a>
     </div>
-  <?php endwhile; endif; ?>
+
+  <?php
+        break;
+      }
+    }
+
+    /* show non featured posts */
+    foreach ( $posts as $post  ) {
+      $post_id = $post->ID;
+      $is_featured = false;
+      $excerpt = get_field( 'excerpt', $post_id );
+
+      if( !has_tag( 'featured', $post ) ) {
+  ?>
+
+    <div class="post">
+      <?php
+        if( has_post_thumbnail( $post_id ) ) {
+          echo get_the_post_thumbnail( $post_id, 'category-image', array( "class" => "post_image" ));
+        }
+      ?>
+
+      <div class="post_meta">
+          <h2 class="post_headline">
+            <?php echo $post->post_title; ?>
+          </h2>
+
+          <div class="post_excerpt richtext">
+            <?php echo $excerpt; ?>
+          </div>
+        </div>
+    </div>
+
+  <?php
+      }
+    }
+  ?>
 </div>
 
 <?php get_footer(); ?>
