34a0c85cfbb143be0354ced855a39ecefcf4729f
Gustav Pursche <pursche@posteo.de>
2018-02-08 23:28:30 +0100

    Remove generator meta tag

--- wordpress/wp-content/themes/jib-collective/functions.php
+++ wordpress/wp-content/themes/jib-collective/functions.php
@@ -127,4 +127,5 @@
   remove_action('wp_head', 'print_emoji_detection_script', 7);
   remove_action('wp_print_styles', 'print_emoji_styles');
   remove_action('wp_head', 'wp_resource_hints', 2);
+  remove_action('wp_head', 'wp_generator');
 ?>
